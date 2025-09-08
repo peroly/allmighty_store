31d0020288d660d5098608e86f58e739cf88a2bd
peroly <mernawatispeed08@gmail.com>
2025-09-08 16:05:05 +0700

    connect ke pws

--- allmighty_store/settings.py
+++ allmighty_store/settings.py
@@ -30,7 +30,7 @@ PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "afero-aqil-AllmightyStore.pbp.cs.ui.ac.id"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "afero-aqil-allmightystore.pbp.cs.ui.ac.id"]
 
 
 # Application definition
